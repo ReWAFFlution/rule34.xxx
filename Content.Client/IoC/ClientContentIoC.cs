34ea3f6007b40830bd3de2c201902b309640cd20
ReWAFFlution <rockstarfly65@gmail.com>
2026-09-26 17:01:12 +0300

    Revert "fix p.1"
    This reverts commit 8ba8cce7ccef5a848044999510a1748ce0b18867.

--- Content.Client/IoC/ClientContentIoC.cs
+++ Content.Client/IoC/ClientContentIoC.cs
@@ -2,7 +2,6 @@
 
 using Content.Client._RMC14.LinkAccount;
 using Content.Client._Arcane.DiscordRoles;
-using Content.Client._Arcane.Trail.Line.Manager;
 using Content.Client.Administration.Managers;
 using Content.Client.Changelog;
 using Content.Client.Chat.Managers;
@@ -70,7 +69,6 @@ namespace Content.Client.IoC
             collection.Register<LinkAccountManager>(); // RMC14
             collection.Register<DiscordRoleManager>(); // Arcane
             collection.Register<ISharedDiscordRoleManager, DiscordRoleManager>(); // Arcane
-            collection.Register<ITrailLineManager, TrailSplineManager>();
             collection.Register<ClientsidePlaytimeTrackingManager>();
         }
     }
